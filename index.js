f9979812f30f03cf8ca7f8c524ac30d7b7177004
Crhistian Caraballo <cristianjcj35@gmail.com>
2021-01-18 18:33:04 -0500

    Welcome

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 // *** versions ***
 // MAJOR.MINOR.PATCH
 // 1.0.0
-const clearConsole = require("clear-any-console");
+const { name, version, description } = require("./package.json");
+const welcome = require("cli-welcome");
 // ls -al see permisions
 // #!/usr/bin/env python
 //  Permision error chmod -x
 // chmod 155 a.txt
 // fakeroot chmod 111 a.txt
-clearConsole();
+
+welcome({
+  title: "Get to know Crhis",
+  description,
+  version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+});
+
 console.log("Hi Crhis");
