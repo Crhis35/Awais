9e07ddc27c9c09832dad4987f4cc01842f2484f3
Crhistian Caraballo <cristianjcj35@gmail.com>
2021-01-18 19:03:32 -0500

    Update lincence

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 // 1.0.0
 const { name, version, description } = require("./package.json");
 const welcome = require("cli-welcome");
+const chalk = require("chalk");
+const log = console.log;
 // ls -al see permisions
 // #!/usr/bin/env python
 //  Permision error chmod -x
@@ -11,13 +13,26 @@ const welcome = require("cli-welcome");
 // fakeroot chmod 111 a.txt
 
 welcome({
-  title: "Get to know Crhis",
+  title: ` Crhistian Caraballo `,
+  tagLine: `Howdy, this is so cool`,
   description,
   version,
-  bgColor: `#FADC00`,
-  color: `#000000`,
+  bgColor: `#6A44FF`,
+  color: `#2A157E`,
   bold: true,
   clear: true,
 });
 
-console.log("Hi Crhis");
+log(
+  `
+  ${chalk.italic(`Award-winning open-source engineer and developer advocate. 
+  Author of various open-source dev-tools and software libraries used by 
+  millions of developers worldwide. Leading developers and publishing 
+  technical content for over a decade. Self-confessed tech 
+  comedian cracking silly jokes in the JavaScript web and cloud communities.`)}
+
+  ${chalk.hex(`#1da`).bold.inverse(`  Github  `)}: ${chalk.dim(
+    `https://github.com/Crhis35`
+  )}
+  `
+);
